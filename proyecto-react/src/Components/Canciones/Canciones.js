2a3c1bfd47be9caf285ea51c4449c7ddf8bc6b7f
Sofia Cortes <sofiacortes@MBP-de-Ariel-2.fibertel.com.ar>
2022-04-05 18:46:34 -0300

    creamos el hijo y el map del padre

--- proyecto-react/src/Components/Canciones/Canciones.js
+++ proyecto-react/src/Components/Canciones/Canciones.js
@@ -1,9 +1,45 @@
 import React, {Component} from 'react'; 
 
 class Canciones extends Component{
-
+    constructor(props){
+        super(props);
+        this.state={
+            datos: [],
+        }
+    }
+    
 
     
+
+    
+    render(){
+        console.log(this.props);
+        return(
+            <div>
+                <article>
+                                <main>
+                                    <img src="./img/image-default.png" alt=""/> 
+                                     <h3>Título/ Nombre</h3>
+                                     <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint cumque velit minus facere laboriosam voluptatem impedit ea unde labore optio eius quis, dignissimos expedita. Culpa, soluta perspiciatis! Sint, laboriosam cum.</p> 
+                                     <section class="aditional-info"> 
+                                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse qui atque.</p> 
+                                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse qui atque.</p>
+                                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse qui atque.</p> 
+                                      </section>
+                                      <a href="">Ver más</a>
+                                </main>
+                 </article> 
+            </div>
+
+        )
+    }
+    
+
+
+
+
+
 }
+
 
 export default Canciones; 
--- proyecto-react/src/Components/Playlist/Playlist.js
+++ proyecto-react/src/Components/Playlist/Playlist.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'; 
-
+import Canciones from "../Canciones/Canciones"
 class Playlist extends Component{
     constructor(props){
         super(props);
@@ -10,7 +10,7 @@ class Playlist extends Component{
     
 
     componentDidMount(){
-        let url = 'https://thingproxy.freeboard.io/fetch/https://developers.deezer.com/api/explorer?url=chart/0/tracks '
+        let url = 'https://thingproxy.freeboard.io/fetch/https://api.deezer.com/chart/0/tracks&top?limit=10'
         fetch(url)
         .then(response => response.json()) // transofrma los datos que vienen de la API en Json
         .then( info => this.setState({
@@ -24,17 +24,18 @@ class Playlist extends Component{
         console.log(this.state.datos);
         return(
             <div>
-                {this.state.datos === ''?
-                <h3> Cargando... </h3> : 
-                <h1> Llego bien </h1> }
-                {/* // this.state.datos.map(datos)} */}
+                {this.state.datos === ''? // if ternario: creamos un condicional porque si tarta en traer la info de la api, que me tire cargando, y sino que me traiga del estado el array con toda la info de las canciones.
+                <h3> Cargando... </h3> :  
+                
+                this.state.datos.map((cancion, idx) => <Canciones key={cancion.datos + idx} 
+                artistas={cancion} />)  }
+               
             </div>
 
         )
+
     }
     
-
-
 
 
 
