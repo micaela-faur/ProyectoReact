ea685a78fe295eb3b67e3f9be41f4cda42cbad44
sofiacortes1 <scortes@udesa.edu.ar>
2022-04-06 12:44:25 -0300

    funcionalidad borrar e hijo props y api

--- proyecto-react/src/Components/Canciones/Canciones.js
+++ proyecto-react/src/Components/Canciones/Canciones.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'; 
+import './Canciones.css'
 
 class Canciones extends Component{
     constructor(props){
@@ -9,7 +10,6 @@ class Canciones extends Component{
     }
     
 
-    
 
     
     render(){
@@ -18,15 +18,16 @@ class Canciones extends Component{
             <div>
                 <article>
                                 <main>
-                                    <img src="./img/image-default.png" alt=""/> 
-                                     <h3>Título/ Nombre</h3>
-                                     <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint cumque velit minus facere laboriosam voluptatem impedit ea unde labore optio eius quis, dignissimos expedita. Culpa, soluta perspiciatis! Sint, laboriosam cum.</p> 
-                                     <section class="aditional-info"> 
+                                    <img src={this.props.artistas.artist.picture} alt={this.props.artistas} /> 
+                                     <h3>Título: {this.props.artistas.title} </h3>
+                                     <h3>Artista: {this.props.artistas.artist.name} </h3>
+                                      <button> Ver más </button> 
+                                      <section class="aditional-info"> 
                                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse qui atque.</p> 
                                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse qui atque.</p>
                                       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse qui atque.</p> 
                                       </section>
-                                      <a href="">Ver más</a>
+                                      <button type="button" onClick={()=> this.props.borrarCancion(this.props.artistas.id)}>Eliminar</button>
                                 </main>
                  </article> 
             </div>
--- proyecto-react/src/Components/Playlist/Playlist.js
+++ proyecto-react/src/Components/Playlist/Playlist.js
@@ -1,16 +1,22 @@
 import React, {Component} from 'react'; 
-import Canciones from "../Canciones/Canciones"
+import Canciones from "../Canciones/Canciones";
+import './Playlist.css';
+
+
+
+
 class Playlist extends Component{
     constructor(props){
         super(props);
         this.state={
             datos: [],
+            limit: 20
         }
     }
     
 
     componentDidMount(){
-        let url = 'https://thingproxy.freeboard.io/fetch/https://api.deezer.com/chart/0/tracks&top?limit=10'
+        let url = 'https://thingproxy.freeboard.io/fetch/https://api.deezer.com/chart/0/tracks&top?limit' + this.state.limit
         fetch(url)
         .then(response => response.json()) // transofrma los datos que vienen de la API en Json
         .then( info => this.setState({
@@ -19,16 +25,27 @@ class Playlist extends Component{
         .catch(error => console.log(error))
     } 
 
-    
+   
+
+    borrar(id){
+        let cancionesFiltradas = []; // creamos un array para cargar las canciones filtradas
+     cancionesFiltradas = this.state.datos.filter( unaCancion => unaCancion.id !== id );
+
+     this.setState({
+         datos: cancionesFiltradas // decimos que las canciones filtradas se carguen en el array segun la condicion del filter
+     })
+
+
+    }
+
     render(){
         console.log(this.state.datos);
         return(
-            <div>
-                {this.state.datos === ''? // if ternario: creamos un condicional porque si tarta en traer la info de la api, que me tire cargando, y sino que me traiga del estado el array con toda la info de las canciones.
+            <div className = 'container' >
+                {this.state.datos === '' ? // if ternario: creamos un condicional porque si tarta en traer la info de la api, que me tire cargando, y sino que me traiga del estado el array con toda la info de las canciones.
                 <h3> Cargando... </h3> :  
-                
                 this.state.datos.map((cancion, idx) => <Canciones key={cancion.datos + idx} 
-                artistas={cancion} />)  }
+                artistas={cancion} borrarCancion = { (id)=> this.borrar(id)} />)  }
                
             </div>
 
